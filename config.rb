3becfeebda7409e77d4b587a469f337263be2a4f
Anthony Gibson <anthony@vaultlabs.com>
2017-05-25 22:27:28 -0700

    deploy testing

--- config.rb
+++ config.rb
@@ -68,12 +68,3 @@ activate :minify_html
 # end
 
 # Github deploy settings, find others here: https://github.com/middleman-contrib/middleman-deploy
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  deploy.build_before = true
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-end
