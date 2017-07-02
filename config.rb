e480726ad0d7a836a24f65f25097c6cce9e4ba34
Anthony Gibson <anthony@vaultlabs.com>
2017-07-02 15:15:05 -0700

    add new defaults

--- config.rb
+++ config.rb
@@ -37,10 +37,14 @@ page '/*.txt', layout: false
 #   end
 # end
 
+configure :development do
+  activate :livereload, no_swf: true
+end
+
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
