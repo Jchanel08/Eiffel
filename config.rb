daf0c59ffa5519f3b0c30b959206430782f03766
Anthony Gibson <anthony@vaultlabs.com>
2017-08-19 17:11:34 -0700

    cleanup config, adjust layout

--- config.rb
+++ config.rb
@@ -15,30 +15,6 @@ page '/*.txt', layout: false
 # page '/path/to/file.html', layout: 'other_layout'
 
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-
 # External Asset Pipeline
 activate :external_pipeline,
   name: :webpack,
@@ -58,18 +34,6 @@ end
 # configure :build do
 #   activate :minify_css
 #   activate :minify_javascript
-# end
-
-
-# CDN configuration
-# configure :build do
-#  activate :minify_css
-#  activate :minify_javascript
-
-# Append a hash to asset urls (make sure to use the url helpers)
-#  activate :asset_hash
-
-#  activate :asset_host, :host => '//YOURDOMAIN.cloudfront.net'
 # end
 
 
