378bc0194d4002b9ae27aa06cef78268845d513f
Anthony Gibson <iamanthonygibson@gmail.com>
2019-01-26 10:15:42 -0800

    update github username

--- config.rb
+++ config.rb
@@ -28,6 +28,7 @@ activate :external_pipeline,
   source: ".tmp/dist",
   latency: 1
 
+set :markdown_engine, :redcarpet
 set :css_dir, 'assets/css'
 set :js_dir, 'assets/js'
 set :images_dir, 'images'
